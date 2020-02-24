7d365aad91a1b3772495d0e72b27eed914de7409
Ariane Evans <ariane.evans@student.makeschool.com>
2020-02-24 15:59:04 -0800

    [add] test

--- wiki/forms.py
+++ wiki/forms.py
@@ -4,9 +4,6 @@ from wiki.models import Page
 
 class PageForm(forms.ModelForm):
     """ Render and process a form based on the Page model. """
-    model = Page
-
-
-class FriendlyForm(forms.Form):
-    post_title = forms.CharField(max_length=250)
-    author_name = forms.CharField(max_length=100)
+    class Meta:
+        model = Page
+        fields = '__all__'
--- wiki/views.py
+++ wiki/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
 from wiki.models import Page
+from wiki.forms import PageForm
 
 class PageListView(ListView):
     """ Renders a list of all Pages. """
@@ -25,3 +29,15 @@ class PageDetailView(DetailView):
         return render(request, 'page.html', {
           'page': page
         })
+
+class PageCreateView(CreateView):
+  def get(self, request, *args, **kwargs):
+    context = {'form': PageForm()}
+    return render(request, 'books/new.html', context)
+
+  def post(self, request, *args, **kwargs):
+    form = PageForm(request.POST)
+    if form.is_valid():
+      page = form.save()
+      return HttpResponseRedirect(reverse_lazy('page:detail', args=[page.id]))
+    return render(request, 'page/new.html', {'form':form})
