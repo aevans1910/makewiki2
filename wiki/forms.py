ff4fca1f66dd14230b7e6c9555e5d315ce5bfec9
Ariane Evans <ariane.evans@student.makeschool.com>
2020-02-24 13:06:50 -0800

    stuff

--- wiki/forms.py
+++ wiki/forms.py
@@ -5,3 +5,8 @@ from wiki.models import Page
 class PageForm(forms.ModelForm):
     """ Render and process a form based on the Page model. """
     model = Page
+
+
+class FriendlyForm(forms.Form):
+    post_title = forms.CharField(max_length=250)
+    author_name = forms.CharField(max_length=100)
